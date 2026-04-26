f2a33a7b6c4a377e7d11c151aafe389636eeb3be
Jan Galinski <jan.galinski@holisticon.de>
2026-04-26 09:59:59 +0200

    move yarn file

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,10 +1,20 @@
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.ktx.compose)
   alias(libs.plugins.jetbrains.compose)
   alias(libs.plugins.kobweb.application)
+}
+
+kobweb {
+  pagesPackage = "io.github.bstdoom.tagessieg.site.pages"
+}
+
+rootProject.plugins.withType<YarnPlugin> {
+  rootProject.extensions.getByType<YarnRootExtension>().lockFileDirectory = rootProject.file("gradle/kotlin-js-store")
 }
 
 kotlin {
